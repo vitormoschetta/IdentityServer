98e9687055f63cbd31166831575dbb227afd2fc3
vitormoschetta <vitormoschetta@gmail.com>
2020-11-13 18:38:11 -0400

    update

---
+++ TestES/ApiResource/Controllers/PublicController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiResource.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PublicController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> objeto()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ TestES/ServerAuth/Config.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4.Test;
+
+namespace ServerAuth
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>                          // <-- Recursos/APIs que desejamos proteger 
+        {
+            new ApiScope("api01", "My API")
+        };
+
+        public static IEnumerable<Client> Clients =>
+           new List<Client>                         // <-- Clientes/App que podem acessar nossas (determinadas) APIs protegidas
+           {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api01" }     // <-- Recursos/APIs ao qual este cliente terá acesso
+                }
+           };
+
+        public static List<TestUser> GetUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+
+    }
+}
